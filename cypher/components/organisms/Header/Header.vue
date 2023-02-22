ff0ae02e0e8fdd3dce952ae16138d083b18d5877
k_ozawa <kouta.ozawa@nadia.bz>
2023-02-22 14:00:13 +0900

    add header ~ only PC

--- cypher/components/organisms/Header/Header.vue
+++ cypher/components/organisms/Header/Header.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="l-header">header</div>
+  <div class="l-header">
+    <!-- <div class="menu-btn is-sp">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div> -->
+    <nav class="nav">
+      <ul class="nav-list">
+        <li><a href="">ホーム</a></li>
+        <li><a href="">私達について</a></li>
+        <li><a href="">選ばれる理由</a></li>
+        <li><a href="">ワークフロー</a></li>
+        <li><a href="">FAQ</a></li>
+        <li><a href="">事例紹介</a></li>
+        <li><a href="">会社概要</a></li>
+        <li><a href="">お問合せ・お見積</a></li>
+        <li><a href="">個人情報保護法について</a></li>
+      </ul>
+      <!-- <div class="nav-sns is-sp">
+        <a href="" class="nav-sns__icon">
+          <img src="@/assets/gvg/icon-facebook.svg" alt="facebook" />
+        </a>
+        <a href="" class="nav-sns__icon">
+          <img src="@/assets/gvg/icon-instagram.svg" alt="instagram" />
+        </a>
+        <a href="" class="nav-sns__icon">
+          <img src="@/assets/gvg/icon-pintarest.svg" alt="pintarest" />
+        </a>
+      </div> -->
+    </nav>
+  </div>
 </template>
 
 <script>
