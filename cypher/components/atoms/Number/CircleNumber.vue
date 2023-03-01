a82936869d87c4c5a65a548f2559f0cc14533b0b
yutamotegi <moyuta0408@gmail.com>
2023-03-01 20:09:21 +0900

    feat: philosophy

---
+++ cypher/components/atoms/Number/CircleNumber.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="circle-number">
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: { type: String, default: "1" },
+  },
+};
+</script>
+  
+  <style lang="scss" scoped>
+.circle-number {
+  background: gray;
+  color: white;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
